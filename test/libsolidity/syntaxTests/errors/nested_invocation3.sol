8d338da5a88af2cdd118d343410931285b3cb2ba
chriseth <chris@ethereum.org>
2021-02-08 18:46:17 +0100

    syntax tests

--- test/libsolidity/syntaxTests/errors/nested_invocation3.sol
+++ test/libsolidity/syntaxTests/errors/nested_invocation3.sol
@@ -1,7 +1,7 @@
-error E1(uint);
+error E1();
 error E2();
 function f() pure {
-    revert(E1(E2));
+    revert(E1(E2()));
 }
 // ----
 // TypeError 9553: (62-64): Invalid type for argument in function call. Invalid implicit conversion from function () pure to uint256 requested.
